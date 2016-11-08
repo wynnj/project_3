b65e2688abc09a2547c2f305e3b4a67fed3b8200
wynnj <justin141410@gmail.com>
2016-11-08 09:44:15 -0500

    Pie Chart in but wont load

--- js/usage.js
+++ js/usage.js
@@ -1,47 +1,22 @@
-$(document).ready(function() {
-    console.log("ready!");
+var jsonData = [
+    {name: 'Bathroom', upload: 70},
+    {name: 'Other', upload: 30},
+]
 
-    loadData();
+var data = {};
+var sites = [];
+jsonData.forEach(function(e) {
+    sites.push(e.name);
+    data[e.name] = e.upload;
+})
 
-    function loadData() {
-        $.ajax({
-            method: "GET",
-            url: "js/data.json",
-            dataType: "json",
-            success: parseData
-        });
-    }
-
-    function parseData(data) {
-      console.log(data);
-      var chart = c3.generate({
-          data: {
-            columns: [
-          ['data1', 75],
-          ['data2', 50],
-        ],
-         type : 'pie',
-         onclick: function (d, i) { console.log("onclick", d, i); },
-         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-     }
- });
-
- setTimeout(function () {
-     chart.load({
-         columns: [
-             ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-             ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-             ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
-         ]
-     });
- }, 1500);
-
- setTimeout(function () {
-     chart.unload({
-         ids: 'data1'
-     });
-     chart.unload({
-         ids: 'data2'
-     });
- }, 2500);
+chart = c3.generate({
+  bindto: '#chart',
+    data: {
+        json: [ data ],
+        keys: {
+            value: sites,
+        },
+        type:'pie'
+    },
+});
